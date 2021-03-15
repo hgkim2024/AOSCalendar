cb7f6b6f14b6e9cd2c664d9155a4b655c11f4792
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-03-15 10:04:08 +0900

    rx 라이브러리 추가

--- app/src/main/java/com/asusoft/calendar/util/recyclerview/holder/addeventholder/edittext/EditTextHolder.kt
+++ app/src/main/java/com/asusoft/calendar/util/recyclerview/holder/addeventholder/edittext/EditTextHolder.kt
@@ -2,10 +2,14 @@ package com.asusoft.calendar.util.recyclerview.holder.addeventholder.edittext
 
 import android.content.Context
 import android.view.View
-import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import com.asusoft.calendar.R
+import com.asusoft.calendar.util.extension.ExtendedEditText
 import com.asusoft.calendar.util.recyclerview.RecyclerViewAdapter
+import com.jakewharton.rxbinding4.widget.textChanges
+import com.orhanobut.logger.Logger
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class EditTextHolder(
         val context: Context,
@@ -17,17 +21,19 @@ class EditTextHolder(
         if (adapter.list[position] is EditTextItem) {
             val item = adapter.list[position] as EditTextItem
 
-            val tvEdit = view.findViewById<EditText>(R.id.tv_edit)
+            val tvEdit = view.findViewById<ExtendedEditText>(R.id.tv_edit)
             tvEdit.hint = item.hint
-
-            for (otherItem in adapter.list) {
-                if (otherItem is EditTextItem) {
-                    tvEdit.removeTextChangedListener(otherItem.textWatcher)
-                }
-            }
+            tvEdit.clearTextChangedListeners()
 
             tvEdit.setText(item.context)
             tvEdit.addTextChangedListener(item.textWatcher)
+//
+//            tvEdit.textChanges()
+//                .debounce(500, TimeUnit.MILLISECONDS)
+//                .subscribeOn(Schedulers.io())
+//                .subscribe { charSequence ->
+//                    Logger.d("Text Change!!: ${charSequence.toString()}")
+//                }
         }
     }
 }
