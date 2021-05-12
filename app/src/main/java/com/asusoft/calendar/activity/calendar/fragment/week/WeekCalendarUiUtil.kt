1ab12e71dd827afe786d7b4daafa733e6e37af5e
DESKTOP-A43T7EC\Asu <asukim2020@gmail.com>
2021-05-12 15:58:25 +0900

    주간 캘린더 Empty 메시지가 가려지는 오류 수정

--- app/src/main/java/com/asusoft/calendar/activity/calendar/fragment/week/WeekCalendarUiUtil.kt
+++ app/src/main/java/com/asusoft/calendar/activity/calendar/fragment/week/WeekCalendarUiUtil.kt
@@ -76,8 +76,6 @@ object WeekCalendarUiUtil {
             }
 
             vw.tag = date.time
-            vw.setBackgroundColor(CalendarApplication.getColor(R.color.background))
-
             vw.layoutParams = ConstraintLayout.LayoutParams(
                     0,
                     0
